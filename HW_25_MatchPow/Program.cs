430f3ad271ad69d8acca7f8f1e52b5a85f9d40b4
Olesya <H_lesya00@mail.ru>
2022-08-04 23:21:23 +0300

    MatchPow

---
+++ HW_25_MatchPow/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите два числа: "); 
+int A = int.Parse(Console.ReadLine()); 
+int B = int.Parse(Console.ReadLine()); 
+ 
+Console.WriteLine("Возведение в степень = " + Math.Pow((A),B)); 
+ 
+double stepDigit (int first, int second ) 
+{ 
+    double result = Math.Pow((first),second); 
+    return result; 
+} 
