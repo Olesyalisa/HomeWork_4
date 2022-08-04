bc3884f05f6c37acb41bdd03c730c7a1ddad7556
Olesya <H_lesya00@mail.ru>
2022-08-04 23:23:32 +0300

    User

---
+++ HW_29_UserRun/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] getMassive(int num) 
+{ 
+    int [] arr = new int[num];  
+    Random rnd = new Random(); 
+ 
+    for (int i = 0; i<arr.Length; i++) 
+    { 
+         arr[i] = rnd.Next(0, 100);   
+    } 
+    return arr; 
+} 
+ 
+Console.WriteLine("Введите размер массива = ");
+int size = int.Parse(Console.ReadLine());
+
+int[] mas = getMassive(size); 
+ 
+for (int i = 0; i<mas.Length; i++) 
+{ 
+    Console.Write(mas[i] + " "); 
+}
