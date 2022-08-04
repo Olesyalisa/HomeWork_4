3d0a5d8a20a1e4c4f3fc9d51ad9974ed5d2391aa
Olesya <H_lesya00@mail.ru>
2022-08-04 23:21:59 +0300

    HW_27

---
+++ HW_27/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. 
+452 -> 11 
+82 -> 10 
+9012 -> 12 */ 
+ 
+Console.WriteLine("Введите число: "); 
+int number = int.Parse(Console.ReadLine()); 
+int res = 0; 
+ 
+Console.WriteLine(sumNumbers(res)); 
+int sumNumbers (int sum) 
+{ 
+    while (number > 0) 
+    {  
+        res += number % 10; 
+        number = number /10; 
+    } 
+    return res; 
+}
